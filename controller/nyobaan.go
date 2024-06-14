da08c59da131169cf70867d9995e377891cd333e
jul003 <dzulkiflifaiz11@gmail.com>
2024-06-14 15:15:41 +0700

    putswag

--- controller/nyobaan.go
+++ controller/nyobaan.go
@@ -104,7 +104,18 @@ func InsertDataPresensi(c *fiber.Ctx) error {
 		"inserted_id": insertedID,
 	})
 }
-
+// UpdateData godoc
+// @Summary Update data presensi.
+// @Description Ubah data presensi.
+// @Tags Presensi
+// @Accept json
+// @Produce json
+// @Param id path string true "Masukan ID"
+// @Param request body ReqPresensi true "Payload Body [RAW]"
+// @Success 200 {object} Presensi
+// @Failure 400
+// @Failure 500
+// @Router /update/{id} [put]
 func UpdateData(c *fiber.Ctx) error {
 	db := config.Ulbimongoconn
 
